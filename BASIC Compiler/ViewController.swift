0d32b364d80c31451ec90867558aa8daf25bfdcd
HMalonzo <Hanniel@Hanniels-MacBook-Pro.local>
2016-05-25 21:41:55 -0500

    new

---
+++ BASIC Compiler/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  BASIC Compiler
+//
+//  Created by Hanniel C. Malonzo on 5/16/16.
+//  Copyright © 2016 Hanniel C. Malonzo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var fileView: UITextView!
+    @IBOutlet weak var consoleView: UITextView!
+    
+    var saveFile = ""
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = UIColor(red: 0.45, green: 0.45, blue: 0.45, alpha: 1)
+        
+        
+        
+        
+        
+        
+    }
+    
+    
+    
+    
+    //when the text value changes
+    //save text file
+    //save date and time of edit
+    //execute program
+    
+    
+    
+    
+}
+
